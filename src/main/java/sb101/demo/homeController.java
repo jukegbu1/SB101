a56f92bd515eecb0053e98e80dd483c263632f64
jukegbu1 <jukegbu1@gmail.com>
2019-05-06 18:28:02 -0400

    Initial commit

---
+++ src/main/java/sb101/demo/homeController.java
@@ -0,0 +1,14 @@
+package sb101.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+    @RequestMapping("/")
+    public String homePage(){
+        return "index";
+    }
+
+}
